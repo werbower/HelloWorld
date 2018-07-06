0e74965a7121ac1d1433d50647fa3fbccd95d475
werbower <werbower@gmail.com>
2018-07-06 16:46:17 +0300

    begin

---
+++ HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService : IHelloWorldService {
+        public string GetMessage(string name) {
+            return $"Hello, {name}";
+        }
+    }
+}
---
+++ HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HelloWorldService {
+    [ServiceContract]
+    public interface IHelloWorldService {
+        [OperationContract]
+        string GetMessage(string name);
+    }
+}
